9d1cc0b708e88404103da4990d14157c0a97bc0d
houjityaumai <houjitya1910@gmail.com>
2023-10-02 14:26:24 +0900

    create sreach addresscordsrech

--- js/main.js
+++ js/main.js
@@ -1,31 +1,39 @@
 $(function(){
   let $address = $(".address"),
-      form = document.querySelector("form")
-
-  // apiリクエスト
-  const Http = new XMLHttpRequest();
-  const url='https://zipcloud.ibsnet.co.jp/api/search?zipcode=7830060';
-  Http.open("GET", url);
-  Http.send();
-
-  // Ajax
-  Http.onreadystatechange = (e) => {
-    console.log(Http.responseText)
-  }
+      form = document.querySelector("form"),
+      val = null
+      console.log(form)
 
   // ボタンをクリックしたときにテキストを取得
-  function click(){
+  $(document).ready(function(){
     $("#button").on("click", function(){
-      let val = $("#address_text").val();
-      console.log(val);
+      val = $("#address_text").val();
+      // apiリクエスト
+      const Http = new XMLHttpRequest(); //httpやhttpsを通じてサーバーと通信するためのjavascriptのapi 
+      const url='https://zipcloud.ibsnet.co.jp/api/search?zipcode=' + `${val}`;
+      console.log(val)
+      console.log(url)
+      Http.open("GET", url);
+      Http.send();
+
+      // Ajax httpリクエストの状態が変化したときに呼び出されるイベントハンドラ
+      Http.onreadystatechange = (e) => {
+        console.log(Http.responseText)
+        let jsostrings = Http.responseText, //文字列として取得
+            jsondata = JSON.parse(jsostrings), //オブジェクトとして取得
+            jsons = jsondata.results[0],
+            address = `${jsons.address1}` + `${jsons.address2}` + `${jsons.address3}` + `${jsons.prefcode}`
+
+        console.log(address)
+
+      }
     });
-  };
+  })
+
 
   // submitでページを遷移させないようにする
   form.addEventListener("submit", function(event){
     event.preventDefault();
   })
 
-
-  click();
 })
