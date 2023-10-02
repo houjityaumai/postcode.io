75617891691ae6b55d763d15197688523932d311
houjityaumai <houjitya1910@gmail.com>
2023-10-02 11:28:18 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(function(){
+  const Http = new XMLHttpRequest();
+  // const url='https://zipcloud.ibsnet.co.jp/api/search?zipcode=7830060';
+  // Http.open("GET", url);
+  // Http.send();
+
+  // Http.onreadystatechange = (e) => {
+  //   console.log(Http.responseText)
+  // }
+
+  $address = $(".address")
+  $address.text("aaa")
+})
